52daa2ab85b18248bd2b2b5cc49a4504266ca24e
Kyle McAlpine <k@kylejm.dev>
2022-01-09 15:11:30 +0000

    Adds fast refresh
    which maintains react state on refreshing

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,35 @@
 const prod = process.env.NODE_ENV === 'production';
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
-module.exports = {
-  mode: prod ? 'production' : 'development',
-  entry: './src/index.tsx',
-  output: {
-    path: __dirname + '/dist/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        resolve: {
-          extensions: ['.ts', '.tsx', '.js', '.json'],
+module.exports = (_, argv) => {
+  const mode = argv.mode;
+  const isDevelopment = mode === "development";
+  return {
+    mode: prod ? 'production' : 'development',
+    entry: './src/index.tsx',
+    output: {
+      path: __dirname + '/dist/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          resolve: {
+            extensions: ['.ts', '.tsx', '.js', '.json'],
+          },
+          use: 'ts-loader',
         },
-        use: 'ts-loader',
-      },
-    ]
-  },
-  devtool: prod ? undefined : 'source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'public/index.html',
-    }),
-  ],
+      ]
+    },
+    devtool: prod ? undefined : 'source-map',
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+      }),
+      isDevelopment && new ReactRefreshWebpackPlugin(),
+    ].filter(Boolean),
+  }
 };
